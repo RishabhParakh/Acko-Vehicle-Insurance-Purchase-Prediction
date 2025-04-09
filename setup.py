dacc7ac6ea58285fb6cc1f07bed90e132675b16d
Rishabh Parakh <rishabhparakh@10-17-67-39.dynapool.wireless.nyu.edu>
2025-04-08 20:02:40 -0400

    project structure ready venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rishabh Parakh",
+    author_email="parakhrishabh123@gmail.com",
+    packages=find_packages()
+)
